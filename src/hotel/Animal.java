424b0ec4db9aad4e2c3267d05c9b70b95321331c
DamiaoRobson <damiao.domiciano@ccc.ufcg.edu.br>
2016-08-05 17:07:38 -0300

    Criei a classe animal.

--- src/hotel/Animal.java
+++ src/hotel/Animal.java
@@ -2,4 +2,77 @@ package hotel;
 
 public class Animal {
 
+	private String nome;
+	private String tipo;
+	private int idade;
+
+	public Animal(String nome, String tipo, int idade) throws Exception {
+		chacarInfoAnimal(nome, tipo, idade);
+		this.nome = nome;
+		this.tipo = tipo;
+		this.idade = idade;
+	}
+
+	private void chacarInfoAnimal(String nome, String tipo, int idade) throws Exception {
+		if (nome == null || nome.trim().equalsIgnoreCase("")) {
+			throw new Exception("Nome nulo ou vazio.");
+		} else if (tipo == null || tipo.trim().equalsIgnoreCase("")) {
+			throw new Exception("Tipo nulo ou vazio.");
+		} else if (idade <= 0) {
+			throw new Exception("Idade menor ou igual a zero.");
+		}
+		return;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + idade;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((tipo == null) ? 0 : tipo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object cliente) {
+		if (cliente instanceof Animal) {
+			Animal animal = (Animal) cliente;
+			if (this.getNome().equalsIgnoreCase(animal.getNome())
+					&& this.getTipo().equalsIgnoreCase(animal.getTipo())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Nome = %s, Tipo = %s, Idade = %d", getNome(), getTipo(), getIdade());
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
 }
